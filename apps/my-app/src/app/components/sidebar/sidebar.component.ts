c42a359bc764557076ac607bcb8b1be6306e8c71
Nijat Suleymanov <nicat.suleymanov@gmail.com>
2025-10-01 12:26:40 +0200

    Update sidebar.component.ts

--- apps/my-app/src/app/components/sidebar/sidebar.component.ts
+++ apps/my-app/src/app/components/sidebar/sidebar.component.ts
@@ -3,31 +3,53 @@ import { FileItem } from '../../models/folder.model';
 
 @Component({
   selector: 'app-sidebar',
-  standalone: false,
   template: `
-    <ul class="list-group">
+    <ul class="sidebar-list">
       <ng-container *ngFor="let folder of folders">
-        <li class="list-group-item"
-            [class.active]="folder.id === selectedId"
-            (click)="selectFolder(folder)">
-          <i class="bi bi-folder"></i> {{ folder.name }}
+        <li (click)="toggleFolder(folder, $event)">
+          <i 
+            class="bi"
+            [ngClass]="{
+              'bi-folder2-open': isOpen(folder),
+              'bi-folder2': !isOpen(folder),
+              'folder-icon': true
+            }"
+          ></i>
+          <span (click)="selectFolder(folder, $event)">{{ folder.name }}</span>
         </li>
-        <ul *ngIf="folder.children?.length">
-          <li *ngFor="let sub of folder.children"
-              class="list-group-item"
-              (click)="selectFolder(sub)">
-            <i class="bi bi-folder"></i> {{ sub.name }}
+        <ul *ngIf="folder.children?.length && isOpen(folder)">
+          <li *ngFor="let sub of folder.children" (click)="selectFolder(sub, $event)">
+            <i class="bi bi-folder2 folder-icon"></i>
+            <span>{{ sub.name }}</span>
           </li>
         </ul>
       </ng-container>
     </ul>
-  `
+  `,
+  styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent {
   @Input() folders: FileItem[] | null = [];
   @Input() selectedId: string | null = null;
   @Output() folderSelected = new EventEmitter<string>();
-  selectFolder(folder: FileItem) {
+
+  openFolders = new Set<string>();
+
+  toggleFolder(folder: FileItem, event: Event) {
+    event.stopPropagation();
+    if (this.isOpen(folder)) {
+      this.openFolders.delete(folder.id);
+    } else {
+      this.openFolders.add(folder.id);
+    }
+  }
+
+  isOpen(folder: FileItem): boolean {
+    return this.openFolders.has(folder.id);
+  }
+
+  selectFolder(folder: FileItem, event: Event) {
+    event.stopPropagation();
     this.folderSelected.emit(folder.id);
   }
 }
