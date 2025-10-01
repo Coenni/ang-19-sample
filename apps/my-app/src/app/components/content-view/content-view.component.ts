05760de5cf7e6a178670bb6517e766da0077de65
Nijat Suleymanov <nicat.suleymanov@gmail.com>
2025-10-01 11:56:35 +0200

    Update content-view.component.ts

--- apps/my-app/src/app/components/content-view/content-view.component.ts
+++ apps/my-app/src/app/components/content-view/content-view.component.ts
@@ -3,26 +3,39 @@ import { FileItem } from '../../models/folder.model';
 
 @Component({
   selector: 'app-content-view',
-  standalone: false,
   template: `
     <div>
-      <h4>Contents</h4>
-      <div *ngIf="contents?.length; else empty">
-        <div class="row">
-          <div *ngFor="let item of contents" class="col-md-4 mb-2">
-            <div class="card">
-              <div class="card-body">
-                <i [class]="'bi ' + (item.type === 'folder' ? 'bi-folder' : 'bi-file-earmark')"></i>
-                {{ item.name }}
-              </div>
-            </div>
-          </div>
-        </div>
+      <h4>Files</h4>
+      <div *ngIf="filesOnly.length; else empty">
+        <table class="table">
+          <thead>
+            <tr>
+              <th>File Name</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr *ngFor="let file of filesOnly">
+              <td>{{ file.name }}</td>
+              <td>
+                <button class="btn btn-primary btn-sm" (click)="downloadFile(file)">Download</button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </div>
-      <ng-template #empty>No items in this folder.</ng-template>
+      <ng-template #empty>No files in this folder.</ng-template>
     </div>
   `
 })
 export class ContentViewComponent {
   @Input() contents: FileItem[] | null = [];
+
+  get filesOnly(): FileItem[] {
+    return this.contents?.filter(item => item.type === 'file') ?? [];
+  }
+
+  downloadFile(file: FileItem) {
+    window.open(`http://localhost:3001/files/${file.id}/download`, '_blank');
+  }
 }
