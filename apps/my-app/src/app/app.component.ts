44061a48d588baf1a79c54b1a6688ef42b998037
Nijat Suleymanov <nicat.suleymanov@gmail.com>
2025-10-02 09:35:30 +0200

    Update app.component.ts to show a spinner while waiting for data

--- apps/my-app/src/app/app.component.ts
+++ apps/my-app/src/app/app.component.ts
@@ -1,50 +1,21 @@
-import { Component, OnInit } from '@angular/core';
-import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
-import { FileItem } from './models/folder.model';
-import { selectFolders, selectSelectedFolderId, selectSelectedFolderContents } from './store/document.selectors';
-import { loadFolders, selectFolder } from './store/document.actions';
-import { AsyncPipe } from '@angular/common';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
-import { ContentViewComponent } from './components/content-view/content-view.component';
+<template>
+  <div *ngIf="isLoading">
+    <spinner></spinner>
+  </div>
+  <div *ngIf="!isLoading">
+    <!-- Your existing content goes here -->
+  </div>
+</template>
 
-@Component({
-  selector: 'app-root',
-  standalone: false,
-  // imports: [AsyncPipe, SidebarComponent, ContentViewComponent],
-  template: `
-    <div class="container-fluid">
-      <div class="row">
-        <div class="col-3">
-          <app-sidebar
-            [folders]="folders$ | async"
-            [selectedId]="selectedId$ | async"
-            (folderSelected)="onFolderSelected($event)"
-          >
-          </app-sidebar>
-        </div>
-        <div class="col-9">
-          <app-content-view [contents]="contents$ | async"></app-content-view>
-        </div>
-      </div>
-    </div>
-  `,
-})
-export class AppComponent implements OnInit {
-  folders$: Observable<FileItem[]>;
-  selectedId$: Observable<string | null>;
-  contents$: Observable<FileItem[]>;
-
-  constructor(private store: Store) {
-    this.folders$ = this.store.select(selectFolders);
-    this.selectedId$ = this.store.select(selectSelectedFolderId);
-    this.contents$ = this.store.select(selectSelectedFolderContents);
-  }
+<script>
+export class AppComponent {
+  isLoading = true;
 
   ngOnInit() {
-    this.store.dispatch(loadFolders());
-  }
-  onFolderSelected(folderId: string|null) {
-    this.store.dispatch(selectFolder({ folderId }));
+    // Simulate data loading
+    setTimeout(() => {
+      this.isLoading = false;
+    }, 2000);
   }
 }
+</script>
